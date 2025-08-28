adc01d697bb4b9e14540c87f24cd6ee59e128995
choishen1004-bit <choishen1004@gmail.com>
2025-08-29 00:36:54 +0900

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="p-8">
+      <h1 className="text-3xl font-bold text-center">풍성 홈페이지에 오신 걸 환영합니다!</h1>
+      <p className="mt-4 text-center text-gray-600">
+        여기에 내용을 추가해 보세요 ✨
+      </p>
+    </main>
+  );
+}
